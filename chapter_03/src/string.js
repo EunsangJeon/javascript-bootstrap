b6f421681927958d8a0ab7de4a6daa5891c76b2e
EunsangJeon <eunsang.jeon@samsung.com>
2020-08-31 17:29:43 +0900

    working on string.js

---
+++ chapter_03/src/string.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+let stringOne = 'He said "don\'t do that" to Max';
+let stringTwo = "Windows line 1\r\nWindows line 2";
+
+console.log(stringOne);
+console.log(stringTwo);
+
+const TEMPERATURE = 25.3;
+let message = "The current temperature is " + TEMPERATURE + "\u00b0C";
+
+console.log(message);
+
+message = `The current temperature is ${TEMPERATURE}\u00b0C`;
+
+console.log(message);

2f49810b055ed1f6e68ec1ddd2a61227f6254bf4
EunsangJeon <eunsang.jeon@samsung.com>
2020-08-31 10:42:33 +0900

    gulpfile added to chap3

---
+++ chapter_03/gulpfile.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+const { src, dest, series } = require("gulp");
+const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
+
+function lintTask() {
+  return src(["src/**/*.js", "public/src/**/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format());
+}
+
+function serverTask() {
+  return src("src/**/*.js").pipe(babel()).pipe(dest("dist"));
+}
+
+function clientTask() {
+  return src("public/src/**/*.js").pipe(babel()).pipe(dest("public/dist"));
+}
+
+exports.default = series(lintTask, serverTask, clientTask);

e7592f335eb3aff9d051f8909a7721353d0c6444
Eunsang <nicolaus1207@gmail.com>
2020-09-02 22:09:11 +0900

    started array search

--- chapter_08/src/search.js
+++ chapter_08/src/search.js
@@ -1,1 +1,8 @@
 // indexOf
+const obj1 = {name: 'nick'};
+const arr1 = [ 1, 5, "a", obj1, true, 5, [ 1, 2 ], "9" ];
+
+console.log(arr1.indexOf(5));
+console.log(arr1.lastIndexOf(5));
+console.log(arr1.indexOf({name: 'nick'}));
+console.log(arr1.indexOf(obj1));

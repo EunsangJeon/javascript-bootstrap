cfd1e0e1e59790ba2785629cc3bd4e9e29dd93d0
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-04 16:20:19 +0900

    function theory

---
+++ chapter_13/function.js
@@ -0,0 +1,5 @@
+// function as a subroutine
+// function as a subroutine that returns
+// pure function => no side effects, same return to same parameter(s)
+
+// function is an object, but the type is still function

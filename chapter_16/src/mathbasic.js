fa719dc4a71e07f78b75164b844edc77ff2ab928
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-07 17:58:43 +0900

    workin on math

---
+++ chapter_16/src/mathbasic.js
@@ -0,0 +1,15 @@
+let x = 19.51;
+console.log(x.toFixed(3));
+console.log(x.toFixed(2));
+console.log(x.toFixed(1));
+console.log(x.toFixed(0));
+
+x = 3800.5;
+console.log(x.toExponential(4));
+console.log(x.toExponential(3));
+console.log(x.toExponential(2));
+console.log(x.toExponential(1));
+console.log(x.toExponential(0));
+
+x = 12;
+console.log;

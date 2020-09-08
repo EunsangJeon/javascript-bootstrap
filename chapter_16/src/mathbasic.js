4d1f4dc7624b0083dbba03773c32f65245f45427
EunsangJeon <eunsang.jeon@samsung.com>
2020-09-08 09:09:17 +0900

    basic Math

--- chapter_16/src/mathbasic.js
+++ chapter_16/src/mathbasic.js
@@ -12,4 +12,8 @@ console.log(x.toExponential(1));
 console.log(x.toExponential(0));
 
 x = 12;
-console.log;
+console.log(x.toString());
+console.log(x.toString(10));
+console.log(x.toString(16));
+console.log(x.toString(8));
+console.log(x.toString(2));
